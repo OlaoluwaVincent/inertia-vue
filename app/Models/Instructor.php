d994e3d5d23a5fb3c9c8a37b0d668b4ecd4d1453
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-05-14 16:39:42 +0100

    Seeded Instructors

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -22,6 +22,24 @@ class Instructor extends Model
         'languages'
     ];
 
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
     public function courses()
     {
         return $this->hasMany(Course::class);
--- database/seeders/InstructorSeeder.php
+++ database/seeders/InstructorSeeder.php
@@ -6,6 +6,9 @@ use App\Models\Instructor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Factories\InstructorsFactory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 class InstructorSeeder extends Seeder
 {
@@ -14,6 +17,18 @@ class InstructorSeeder extends Seeder
      */
     public function run(): void
     {
-        Instructor::factory()->count(9)->create();
+        Instructor::factory()->count(1)->create();
+        $instructors = Instructor::all();
+
+        // Loop through each instructor and update the username column
+        foreach ($instructors as $instructor) {
+            // Update the username column with a fake name
+            $instructor->update([
+                'headline' => fake()->realTextBetween(50, 80),
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]);
+        }
     }
 }
